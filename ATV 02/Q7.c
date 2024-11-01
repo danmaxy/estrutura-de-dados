55f5ba21d0f777a58575dfc1b68ff468f7afd1ef
danmaxy <danieljatoba034@hotmail.com>
2024-11-01 00:48:33 -0300

    up

--- ATV 02/Q7.c
+++ ATV 02/Q7.c
@@ -3,7 +3,7 @@ int main(){
     int n;
     printf("Digite o numero:");
     scanf("%d",&n);
-    for (int i = 0; i <= n; i++){
-        printf("%d", i);
+    for (int i = 1; i <= n; i++){
+        printf("%d \n", i);
     }
 }
---
+++ ATV 02/Q8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+    int n = 2;
+    while (n != 1){
+        printf("Digite o numero:");
+        scanf("%d",&n);
+    }
+    printf("fim do programa");
+
+}
